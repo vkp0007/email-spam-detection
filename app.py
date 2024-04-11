e2617b004326435fdc763cbcdb68460bc7c13b76
vkp0007 <157368385+vkp0007@users.noreply.github.com>
2024-04-11 23:11:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pickle
+
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+mnb = pickle.load(open('model.pkl', 'rb'))
+st.title("Email spam detection")
+
+input_email = st.text_input("Enter email/sms")
+if st.button('Predict'):
+    vector_input = tfidf.transform([input_email])  # Convert input_email to a list with a single element
+    result = mnb.predict(vector_input)
+    if result == 1:
+     st.header("Spam")
+    else:
+     st.header("Ham")
